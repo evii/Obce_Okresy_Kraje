b75729934bcbe164436c2e8a39522a0b63e8f7c0
Eva Pavelkova <eva.pavelkova@applifting.cz>
2022-02-23 11:15:06 +0100

    complete models for towns, districts and regions

--- app/src/main/java/example/com/obce_okresy_kraje/MainActivity.kt
+++ app/src/main/java/example/com/obce_okresy_kraje/MainActivity.kt
@@ -1,11 +1,40 @@
 package example.com.obce_okresy_kraje
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.squareup.moshi.Moshi
+import example.com.obce_okresy_kraje.model.TownItems
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+
+    private val job = Job()
+    override val coroutineContext = job + Dispatchers.Main
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        launch { getTowns() }
+    }
+
+    private suspend fun getTowns() {
+
+        val moshi = Moshi.Builder().build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(TownDistrictService.BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+
+        val service = retrofit.create(TownDistrictService::class.java)
+
+        val townsCall: Call<TownItems> = service.getTowns()
     }
 }
--- app/src/main/java/example/com/obce_okresy_kraje/TownDistrictService.kt
+++ app/src/main/java/example/com/obce_okresy_kraje/TownDistrictService.kt
@@ -6,17 +6,17 @@ import example.com.obce_okresy_kraje.model.TownItems
 import retrofit2.Call
 import retrofit2.http.GET
 
-private interface TownDistrictService {
-    @GET("/kraje.json")
+internal interface TownDistrictService {
+    @GET("kraje.json")
     fun getRegions(): Call<RegionItems>
 
-    @GET("/okresy.json")
+    @GET("okresy.json")
     fun getDistricts(): Call<DistrictItems>
 
-    @GET("/obce.json")
+    @GET("obce.json")
     fun getTowns(): Call<TownItems>
 
     companion object {
-        const val BASE_URL = "https://data.mpsv.cz/od/soubory/ciselniky"
+        const val BASE_URL = "https://data.mpsv.cz/od/soubory/ciselniky/"
     }
 }
