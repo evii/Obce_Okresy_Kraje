d08a3c7cc6f87750bc48d08d417a086eab60df6c
Eva <eva.pavelkova@applifting.cz>
2022-03-02 13:08:51 +0100

    build Search Byt Town layout

--- app/src/main/java/example/com/obce_okresy_kraje/ui/MainActivity.kt
+++ app/src/main/java/example/com/obce_okresy_kraje/ui/MainActivity.kt
@@ -4,12 +4,22 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.squareup.moshi.Moshi
@@ -34,7 +44,7 @@ class MainActivity : ComponentActivity(), CoroutineScope {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            setTitle()
+            SearchByTownLayout()
         }
 
         //  setContentView(R.layout.activity_main)
@@ -44,11 +54,56 @@ class MainActivity : ComponentActivity(), CoroutineScope {
 
     @Preview
     @Composable
-    fun setTitle() {
-        Text(
-            stringResource(id = R.string.district_for_town_title),
-            style = MaterialTheme.typography.h4,
+    private fun SearchByTownLayout() {
+        Column(
             modifier = Modifier.padding(all = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            SearchByTownTitle()
+            Spacer(modifier = Modifier.height(40.dp))
+            SearchByTownEditText()
+            Spacer(modifier = Modifier.height(40.dp))
+            SearchByTownResultDistrict()
+            Spacer(modifier = Modifier.height(40.dp))
+            SearchByTownResultRegion()
+        }
+    }
+
+    @Composable
+    private fun SearchByTownTitle() {
+        Text(
+            text = stringResource(id = R.string.district_for_town_title),
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colors.primaryVariant
+        )
+    }
+
+    @Composable
+    private fun SearchByTownEditText() {
+        var town by rememberSaveable { mutableStateOf("") }
+        TextField(
+            value = town,
+            onValueChange = { town = it },
+            label = { Text(stringResource(id = R.string.district_for_town_edit_text_label)) })
+    }
+
+    @Composable
+    private fun SearchByTownResultDistrict() {
+        Text(
+            text = "District",
+            style = MaterialTheme.typography.h6,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colors.primaryVariant
+        )
+    }
+
+    @Composable
+    private fun SearchByTownResultRegion() {
+        Text(
+            text = "Region",
+            style = MaterialTheme.typography.h6,
+            fontWeight = FontWeight.Bold,
             color = MaterialTheme.colors.primaryVariant
         )
     }
