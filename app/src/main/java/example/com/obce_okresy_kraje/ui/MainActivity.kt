9057dc56ed1294fde6f18a60d1dbb537996626d1
Eva <eva.pavelkova@applifting.cz>
2022-03-16 13:27:25 +0100

    add Find button functionality

--- app/src/main/java/example/com/obce_okresy_kraje/ui/MainActivity.kt
+++ app/src/main/java/example/com/obce_okresy_kraje/ui/MainActivity.kt
@@ -1,54 +1,16 @@
 package example.com.obce_okresy_kraje.ui
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.squareup.moshi.Moshi
-import example.com.obce_okresy_kraje.data.network.TownDistrictService
-import example.com.obce_okresy_kraje.domain.models.TownItems
 import example.com.obce_okresy_kraje.ui.SearchByTown.SearchByTownScreen
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
-import retrofit2.Call
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.awaitResponse
-import retrofit2.converter.moshi.MoshiConverterFactory
 
-class MainActivity : ComponentActivity(), CoroutineScope {
-
-    private val job = Job()
-    override val coroutineContext = job + Dispatchers.IO
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SearchByTownScreen()
         }
-
-        launch { getTowns() }
-    }
-
-    private suspend fun getTowns() {
-        val moshi = Moshi.Builder().build()
-        val retrofit = Retrofit.Builder()
-            .baseUrl(TownDistrictService.BASE_URL)
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
-            .build()
-
-        val service = retrofit.create(TownDistrictService::class.java)
-
-        val townsCall: Call<TownItems> = service.getTowns()
-        val response: Response<TownItems> = townsCall.awaitResponse()
-        val towns: TownItems? = if (response.isSuccessful) {
-            response.body()
-        } else {
-            null
-        }
-        val first = towns?.towns?.get(0)?.townName?.name
-        Log.v("call", "firstTown: $first")
     }
 }
--- app/src/main/java/example/com/obce_okresy_kraje/ui/SearchByTown/SearchBytTownScreen.kt
+++ app/src/main/java/example/com/obce_okresy_kraje/ui/SearchByTown/SearchBytTownScreen.kt
@@ -1,32 +1,38 @@
 package example.com.obce_okresy_kraje.ui.SearchByTown
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import example.com.obce_okresy_kraje.R
+import example.com.obce_okresy_kraje.ui.components.FindButton
 import example.com.obce_okresy_kraje.ui.components.ResultText
 import example.com.obce_okresy_kraje.ui.components.SearchEditText
 import example.com.obce_okresy_kraje.ui.components.TitleText
+import example.com.obce_okresy_kraje.ui.components.VerticalSpace
+import kotlinx.coroutines.launch
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Preview
 @Composable
 fun SearchByTownScreen() {
+    val searchByTownViewModel: SearchByTownViewModel = viewModel()
     Column(
         modifier = Modifier.padding(all = 16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         TitleText(R.string.district_for_town_title)
-        Spacer(modifier = Modifier.height(40.dp))
+        VerticalSpace()
         SearchEditText()
-        Spacer(modifier = Modifier.height(40.dp))
+        VerticalSpace()
+        FindButton(onClick = { searchByTownViewModel.launchTowns()}, text = stringResource(id = R.string.district_for_town_button_text))
+        VerticalSpace()
         ResultText("District")
-        Spacer(modifier = Modifier.height(40.dp))
+        VerticalSpace()
         ResultText("Region")
     }
 }
